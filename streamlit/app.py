7c4af8265bd357fdb12b7ee38533a45342cc13ae
parkouralvoil <narutolidasan@gmail.com>
2025-05-12 17:16:35 +0800

    feat: streamlit local webapp

---
+++ streamlit/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+import requests
+
+"""
+To locally host the streamlit website, do the ff:
+
+cd streamlit
+
+streamlit run app.py
+"""
+
+st.write(
+    "Streamlit is also great for more traditional ML use cases like computer vision or NLP. Here's an example of edge detection using OpenCV. 👁️"
+)
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+if uploaded_file:
+    image = Image.open(uploaded_file)
+else:
+    image = Image.open(requests.get("https://picsum.photos/200/120", stream=True).raw)
+
+edges = cv2.Canny(np.array(image), 100, 200)
+tab1, tab2 = st.tabs(["Detected edges", "Original"])
+tab1.image(edges, use_container_width=True)
+tab2.image(image, use_container_width=True)
